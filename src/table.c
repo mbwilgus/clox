e99bb574f0c3395f8b645b783967666cb855f160
Michael Wilgus <m.b.wilgus@gmail.com>
2021-03-04 16:26:37 -0500

    optimize hash table lookups

--- src/table.c
+++ src/table.c
@@ -23,7 +23,7 @@ void freeTable(Table* table)
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
 {
-    uint32_t index   = key->hash % capacity;
+    uint32_t index   = key->hash & (capacity - 1); // same as % 2^n
     Entry* tombstone = NULL;
 
     for (;;) {
@@ -43,7 +43,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -136,7 +136,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length,
     if (table->count == 0)
         return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
 
     for (;;) {
         Entry* entry = &table->entries[index];
@@ -151,7 +151,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length,
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
